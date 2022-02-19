d730c300b5de0b8960b4ab9e85bc4168bdde525d
jiatonghan1118 <72241408+jiatonghan1118@users.noreply.github.com>
2022-02-19 12:21:35 +0800

    Update Elevator.java
    Elevator

--- SYSC3303_L4_G3-main/src/Elevator.java
+++ SYSC3303_L4_G3-main/src/Elevator.java
@@ -3,11 +3,91 @@ import data_structure.*;
 public class Elevator implements Runnable {
     private Scheduler scheduler;
     private boolean workToBeDone = false;
+    elevatorState idle;
+    elevatorState opening;
+    elevatorState moveEle;
+    elevatorState closing;
+    elevatorState reachFloor;
+
+    elevatorState currentState;
+
 
     public Elevator(Scheduler scheduler) {
+        idle=new Idle(this);
+        closing=new Closing(this);
+        opening =new Opening(this);
+        moveEle=new MoveEle(this);
+        reachFloor=new ReachFloor(this);
+
+        currentState =idle;
+
+
         this.scheduler = scheduler;
         scheduler.addElevator(this);
     }
+
+    public Elevator(){
+        idle=new Idle(this);
+        closing=new Closing(this);
+        opening =new Opening(this);
+        moveEle=new MoveEle(this);
+        reachFloor=new ReachFloor(this);
+
+        currentState =idle;
+
+    }
+
+    void setCurrentState(elevatorState newElevatorState){
+        currentState =newElevatorState;
+    }
+
+    public void timerStart() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        currentState.timeIsUp();
+    }
+
+
+    public void openDoor(){
+        currentState.openDoor();
+    }
+
+    public void closeDoor(){
+        currentState.closeDoor();
+    }
+
+    public void receiveRequest(){
+        currentState.receiveRequest();
+    }
+
+    public void reachFloor(int floor){
+        currentState.reachFloor(floor);
+    }
+
+    public elevatorState getCurrentState() {
+        return currentState;
+    }
+
+    public elevatorState getClosing() {
+        return closing;
+    }
+
+    public elevatorState getOpening() {
+        return opening;
+    }
+
+    public elevatorState getEleMove(){
+        return moveEle;
+    }
+
+    public elevatorState getIdle() {
+        return idle;
+    }
+
 
     //Receive message from scheduler
     public void receiveMsg(RequestMsg requestMsg) {
@@ -36,5 +116,10 @@ public class Elevator implements Runnable {
 
             scheduler.getRequest();
         }
+    }
+
+    public static void main(String[] args) {
+        Elevator elevator=new Elevator();
+        elevator.receiveRequest();
     }
 }
