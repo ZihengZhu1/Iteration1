1e4c28d334ddd19fbbb32bb4fd862adc19707749
jiatonghan1118 <72241408+jiatonghan1118@users.noreply.github.com>
2022-03-12 23:46:48 -0500

    Update Elevator.java

--- Iteration2/src/Elevator.java
+++ Iteration2/src/Elevator.java
@@ -1,21 +1,30 @@
 /**This class is a representation of an elevator
  * @author Jiatong Han
  */
-import java.util.ArrayList;
-import java.util.LinkedList;
+import data_structure.*;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.io.*;
-import java.net.*;
-import data_structure.*;
-import data_structure.*;
+import java.util.*;
 
 public class Elevator implements Runnable {
+    public static final int UP = 1;
+    public static final int DOWN = -1;
+    public static final int STILL = 0;
+    private int id;
+    private int currentHeight= STILL;
     private Scheduler scheduler;
+    private int currentDirection;
     private boolean workToBeDone = false;
+    private Timer timer;
+    private TimerTask ring;
+
+    private PriorityQueue<Integer> upQueue=new PriorityQueue();
+    private PriorityQueue<Integer> downQueue=new PriorityQueue<>(Comparator.reverseOrder());
+
     ElevatorState idle;
     ElevatorState opening;
     ElevatorState moveEle;
@@ -23,15 +32,28 @@ public class Elevator implements Runnable {
     ElevatorState reachFloor;
 
     ElevatorState currentState;
+
     private static DatagramPacket receivePacket;
-	private static DatagramSocket sendSocket, receiveSocket;
-	private final int Id = 2;
-	private byte request[] = new byte[40];
-	
+    private static DatagramSocket sendSocket, receiveSocket;
+    private final int Id = 2;
+    private byte request[] = new byte[40];
+
+
     /**
      * Constructor for Elevator class
-     * @param scheduler
+     * @param id
      */
+    public Elevator(int id) {
+        idle=new Idle(this);
+        closing=new Closing(this);
+        opening =new Opening(this);
+        moveEle=new MoveEle(this);
+        reachFloor=new ReachFloor(this);
+
+        currentState =idle;
+        this.id=id;
+    }
+
     public Elevator(Scheduler scheduler) {
         idle=new Idle(this);
         closing=new Closing(this);
@@ -44,19 +66,18 @@ public class Elevator implements Runnable {
         this.scheduler = scheduler;
         scheduler.addElevator(this);
     }
-
-
+  
     /**
      * Overload constructor for Elevator class
      */
     public Elevator(){
-    	try{
-			sendSocket = new DatagramSocket();
-			receiveSocket = new DatagramSocket(Id);
-		}catch (SocketException se){
-			se.printStackTrace();
-			System.exit(1);
-		}
+        try{
+            sendSocket = new DatagramSocket();
+            receiveSocket = new DatagramSocket(Id);
+        }catch (SocketException se){
+            se.printStackTrace();
+            System.exit(1);
+        }
         idle=new Idle(this);
         closing=new Closing(this);
         opening =new Opening(this);
@@ -66,57 +87,58 @@ public class Elevator implements Runnable {
         currentState =idle;
 
     }
-    
+
     private void receviveFromScheduler(){
-		
-		receivePacket = Floor.waitPacket(receiveSocket, "Elevator NO 2");
-		
-		byte[] data = receivePacket.getData();
-		request = data;
-		
-		System.out.println("Elevator sending");
-		System.out.println("Elevator No 2 send user to floor" + request[3]);
-		sendPacket(data, data.length, receivePacket.getAddress(), 23, sendSocket);
-		System.out.println("Elevator has sent");
-	}
-    
+
+        receivePacket = Floor.waitPacket(receiveSocket, "Elevator NO 2");
+
+        byte[] data = receivePacket.getData();
+        request = data;
+
+        System.out.println("Elevator sending");
+        System.out.println("Elevator No 2 send user to floor" + request[3]);
+        sendPacket(data, data.length, receivePacket.getAddress(), 23, sendSocket);
+        System.out.println("Elevator has sent");
+    }
+
     public static void sendPacket(byte[]array, int len, InetAddress destadderss, int port, DatagramSocket socket){
-		
-		DatagramPacket packet = new DatagramPacket(array, len, destadderss, port);
-		
-		System.out.println("The Elevator is sending arrival:");
-		System.out.println("From host: " + packet.getAddress());
-		System.out.println("Destination host port: " + packet.getPort());
-		
-		try{
-			socket.send(packet);
-		} catch (IOException ie){
-			ie.printStackTrace();
-			System.exit(1);
-		}
-		
-		System.out.println("Elevator: sent to scheduler");
-	}
+
+        DatagramPacket packet = new DatagramPacket(array, len, destadderss, port);
+
+        System.out.println("The Elevator is sending arrival:");
+        System.out.println("From host: " + packet.getAddress());
+        System.out.println("Destination host port: " + packet.getPort());
+
+        try{
+            socket.send(packet);
+        } catch (IOException ie){
+            ie.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("Elevator: sent to scheduler");
+    }
     
     /**
      * setCurrentState 
      * @param newElevatorState
      */
-    void setCurrentState(ElevatorState newElevatorState){
+    public void setCurrentState(ElevatorState newElevatorState){
         currentState =newElevatorState;
     }
     
     /**
      * timerStart 
      */
-    public void timerStart() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        currentState.timeIsUp();
+    public void timerStart(int delay) {
+        timer=new Timer();
+        ring =new TimerTask() {
+            @Override
+            public void run() {
+                currentState.timeIsUp();
+            }
+        };
+        timer.schedule(ring,delay);
     }
 
     /**
@@ -136,7 +158,52 @@ public class Elevator implements Runnable {
     /**
      * receiveRequest 
      */
-    public void receiveRequest(){
+    public void receiveRequest(RequestMsg requestMsg){
+        int direction=requestMsg.getMovement();
+        int from=requestMsg.getFrom();
+        int destination=requestMsg.getDestination();
+        if(currentDirection==UP){
+            if(direction==UP){
+                upQueue.add(from);
+                upQueue.add(destination);
+            }else if(direction==DOWN){
+                upQueue.add(from);
+                downQueue.add(destination);
+            }else{
+                System.out.println("ERROR");
+            }
+        }else if(currentDirection==DOWN){
+            if(direction==DOWN){
+                downQueue.add(from);
+                downQueue.add(destination);
+            }else if(direction==UP){
+                downQueue.add(from);
+                upQueue.add(destination);
+            }else{
+                System.out.println("ERROR");
+            }
+        }else{//elevator idle
+            if(from>(getCurrentFloor())){
+                currentDirection=UP;
+                receiveRequest(requestMsg);
+            }else if(from<(currentDirection/10)){
+                currentDirection=DOWN;
+                receiveRequest(requestMsg);
+            }else{
+                if(direction==UP){
+                    upQueue.add(destination);
+                }else if(direction==DOWN){
+                    downQueue.add(destination);
+                }else{
+                    System.out.println("ERROR");
+                }
+            }
+        }
+        System.out.println("Element in upQueue");
+        System.out.println(Arrays.toString(upQueue.toArray()));
+        System.out.println("Element in downQueue");
+        System.out.println(Arrays.toString(downQueue.toArray()));
+
         currentState.receiveRequest();
     }
     
@@ -187,6 +254,14 @@ public class Elevator implements Runnable {
         return idle;
     }
 
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
     /**
      * receiveMsg receives message from the scheduler
      * @param requestMsg
@@ -196,7 +271,7 @@ public class Elevator implements Runnable {
         System.out.println(requestMsg.getFrom());
         System.out.println(requestMsg.getMovement());
         System.out.println(requestMsg.getDestination());
-        report(new RequestMsg(2,2, -1, 3), new ArrivalMessage(3, true));
+        report(new RequestMsg(2, -1, 3), new ArrivalMessage(3, true));
 
     }
 
@@ -213,7 +288,99 @@ public class Elevator implements Runnable {
         System.out.println("Finish request");
 
     }
-    
+
+
+    public void increaseHeight(){
+        currentHeight++;
+        if(currentHeight%10==0){
+            reachFloor(getCurrentFloor());
+        }else{
+            timerStart(200);
+        }
+    }
+
+    public void decreaseHeight(){
+        currentHeight--;
+        if(currentHeight%10==0){
+            reachFloor(getCurrentFloor());
+        }else{
+            timerStart(200);
+        }
+    }
+
+    public int getCurrentHeight() {
+        return currentHeight;
+    }
+
+    /**
+    *convert height to floor
+     */
+    public int getCurrentFloor(){
+        return currentHeight/10;
+    }
+
+
+    public void isDestination(){
+        System.out.println("Current direction is ");
+        System.out.println(getCurrentDirection());
+        System.out.println("Current floor is ");
+        System.out.println(getCurrentHeight()/10);
+
+        if(getCurrentDirection()==UP){
+            if (getCurrentFloor()==upQueue.peek()){
+                upQueue.poll();
+                //up Queue is empty
+                if(upQueue.isEmpty()){
+                    System.out.println("Up queue is empty");
+                    //down queue is empty
+                    if(downQueue.isEmpty()){
+                        currentDirection=STILL;
+                        reportStatus();
+                        currentState=opening;
+                        timerStart(1000);
+                    }else{
+                        System.out.println("Down queue is not empty");
+                        currentDirection=DOWN;
+                        reportStatus();
+                        currentState=opening;
+                        timerStart(1000);
+                    }
+                }
+
+            }else{
+                timerStart(200);
+            }
+        }else if(getCurrentDirection()==DOWN){
+            if (getCurrentFloor()==downQueue.peek()){
+                downQueue.poll();
+                //down Queue is empty
+                if(downQueue.isEmpty()){
+                    //up queue is empty
+                    if(upQueue.isEmpty()){
+                        currentDirection=STILL;
+                        reportStatus();
+                        currentState=opening;
+                        timerStart(1000);
+                    }else{
+                        currentDirection=UP;
+                        reportStatus();
+                        currentState=opening;
+                        timerStart(1000);
+                    }
+                }
+
+            }else{
+                timerStart(200);
+            }
+        }
+
+
+    }
+
+    public void reportStatus(){
+
+    }
+
     /**
      * 
      */
@@ -231,9 +398,6 @@ public class Elevator implements Runnable {
      */
     public static void main(String[] args) {
         Elevator elevator=new Elevator();
-        while(true) {
-        	elevator.receviveFromScheduler();
-        }
-        //elevator.receiveRequest();
+        elevator.receiveRequest(new RequestMsg(5,DOWN,1));
     }
 }
